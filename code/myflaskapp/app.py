25dc0cdecd66afc149514c462aaa8ad3ebc432c1
Saurabh Nandedkar <saurabhnandedkar25@gmail.com>
2023-10-11 07:47:19 +0000

    add number to nav bar

--- code/myflaskapp/app.py
+++ code/myflaskapp/app.py
@@ -1,12 +1,47 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import url_for
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return 'Home Page'
+
+@app.route('/about')
+def about():
+    return 'About Page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return f'User {username}'
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    return f'Post {post_id}'
 
 @app.route('/hello/<name>')
 def hello(name):
     return render_template('Child.html', namechild=name)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'Logging in...'
+    else:
+        return 'Login Page'
+    
+    
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    login_url = url_for('login')
+    if request.method == 'POST':
+        return 'Signing in...'
+    else:
+        return f'Signup Page <br> already signed up <a href={login_url}>Login</a>'
+    
+app.run(debug=True)
